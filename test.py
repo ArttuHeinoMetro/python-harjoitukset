6b2878d4283cf04363c8e23295d80b397960e177
ArttuHeinoMetro <arttu.heino@metropolia.fi>
2026-09-04 09:53:28 +0300

    test

--- test.py
+++ test.py
@@ -1,11 +1,5 @@
-import random
+luku = 20
 
-while True:
-    noppa1 = random.randint(1,3)
-    noppa2 = random.randint(1,3)
-    if noppa1 == 3:
-        if noppa2 == 3:
-            print("Nyt tuli 3 ja 3, lopetetaan")
-            break
-    else:
-        print(f"Nyt tuli {noppa1} ja {noppa2}, jatketaan")
+while luku > 0:
+    print(luku)
+    luku -=2
