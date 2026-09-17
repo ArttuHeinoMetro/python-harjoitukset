9c4de9b489bf41786b6c8dd4929f3ee0439300c7
ArttuHeino <arttu.heino@edu.keuda.fi>
2026-09-17 19:53:47 +0300

    peliprojektin edistymistä

--- peliprojekti/peliprojekti.py
+++ peliprojekti/peliprojekti.py
@@ -1,5 +1,7 @@
 import random
 
+kalapaikat = ["Joki", "Järvi", "Meri"]
+
 nimi = input("Kerro Nimesi: ")
 ika = int(input("Kerro Ikäsi: "))
 
@@ -9,14 +11,48 @@ print("")
 print("Tervetuloa Arskan Peliin!")
 print("")
 
+
+#Käyttäjän tiedot ja "päävalikko"
+
 while ika >= 12:
     komento = input("Anna komento:\n")
     if komento == "info":
         print(nimi)
         print(ika)
+        print()
     elif komento == "lopeta":
         break
     elif komento == "random":
         print(random.randint(1,100))
+        print()
+    elif komento == "aloita":
+        print("Aloitetaan peli!")
+        break
 if ika < 12:
     print("Alaikäinen käyttäjä havaittu!")
+
+#Kalastusluvan osto
+def lupa(vastaus):
+    if vastaus == "y":
+        return "Viisas valinta!"
+    else:
+        return "Riskivalinta!"
+
+print(lupa(input("Ostatko kalastusluvan seikkailullesi?(y/n)\n")))
+print()
+
+#kalastuspaikan valinta
+
+def paikkavalinta(valinta):
+    if valinta == "Joki":
+        return "Joki on rauhallinen paikka missä voit rentoutua juoksevan veden äärellä"
+    elif valinta == "Järvi":
+        return "Järviä on Suomessa kaikkialla! Löydät varmasti mieluisan"
+    elif valinta == "Meri":
+        return "Merestä voit saada todella suuria kaloja!"
+    else:
+        return "Virhe"
+
+print(f"Tässä lista eri kalapaikoista:\n {kalapaikat}")
+print()
+print(paikkavalinta(input("Minkä paikan valitset?\n")))
--- peliprojekti/redme.md
+++ peliprojekti/redme.md
@@ -1,2 +1,4 @@
 # Arskan peli
 **Arttu Heino**
+
+Pelin ideana on kalastus seikkailupeli, jossa käyttäjä vaikuttaa ympäristöönsä eri päätöksillä. peliin pitää lisätä lupatarkastaja ja roskia vesissä mitkä vaikuttavat lopputulokseen
